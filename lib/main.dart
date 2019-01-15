7bd2d122ae8674a0f283c560ab9ea2725017e1be
reply <jingjingdexue@gmail.com>
2019-01-15 15:32:40 +0800

    1. 动画完成

--- lib/main.dart
+++ lib/main.dart
@@ -62,17 +62,72 @@ class GuillotineMenu extends StatefulWidget {
   }
 }
 
-class _GuillotineMenuState extends State<GuillotineMenu> {
+///Menu animation status
+enum _GuillotineAnimationStatus{closed, open, animating}
+
+class _GuillotineMenuState extends State<GuillotineMenu>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationControllerMenu;
+  Animation<double> animationMenu;
+  Animation _rotateAnimation;
   double rotationAngle = 0.0;
+  double screenHeight;
+  double screenWidth;
+  _GuillotineAnimationStatus menuAnimationStatus = _GuillotineAnimationStatus.closed;
+
+
+  ///Menu Icon onPress() handle
+  _handleMenuOpenClose() {
+    if (menuAnimationStatus == _GuillotineAnimationStatus.closed){
+      animationControllerMenu.forward().orCancel;
+    } else if(menuAnimationStatus == _GuillotineAnimationStatus.open){
+      animationControllerMenu.reverse().orCancel;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationControllerMenu =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 1000))
+          ..addListener(() {
+            setState(() {});
+          })..addStatusListener((AnimationStatus status){
+            switch (status){
+              case AnimationStatus.completed:
+                menuAnimationStatus = _GuillotineAnimationStatus.open;
+                break;
+              case AnimationStatus.dismissed:
+                menuAnimationStatus = _GuillotineAnimationStatus.closed;
+                break;
+              case AnimationStatus.forward:
+              case AnimationStatus.reverse:
+                menuAnimationStatus = _GuillotineAnimationStatus.animating;
+                break;
+            }
+        });
+    _rotateAnimation = Tween(
+      begin: -pi/2.0,end: 0.0
+    ).animate(animationControllerMenu);
+
+  }
+
+
+  @override
+  void dispose() {
+    animationControllerMenu.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
-    double screenHeight = mediaQueryData.size.height;
-    double screenWidth = mediaQueryData.size.width;
+    screenHeight = mediaQueryData.size.height;
+    screenWidth = mediaQueryData.size.width;
+    double angle = _rotateAnimation.value;
 
     return Transform.rotate(
-      angle: rotationAngle,
+      angle: angle,
       origin: new Offset(24.0, 56.0),
       alignment: Alignment.topLeft,
       child: Material(
@@ -84,6 +139,8 @@ class _GuillotineMenuState extends State<GuillotineMenu> {
           child: Stack(
             children: <Widget>[
               _buildMenuTitle(),
+              _buildMenuIcon(),
+              _buildMenuContent(),
             ],
           ),
         ),
@@ -91,10 +148,8 @@ class _GuillotineMenuState extends State<GuillotineMenu> {
     );
   }
 
+  ///Menu Title
   Widget _buildMenuTitle() {
-    MediaQueryData mediaQueryData = MediaQuery.of(context);
-    double screenHeight = mediaQueryData.size.height;
-    double screenWidth = mediaQueryData.size.width;
     return Positioned(
       top: 32,
       left: 40,
@@ -116,6 +171,64 @@ class _GuillotineMenuState extends State<GuillotineMenu> {
                 letterSpacing: 2.0),
           ),
         ),
+      ),
+    );
+  }
+
+  ///Menu Icon
+  Widget _buildMenuIcon() {
+    return Positioned(
+      child: IconButton(icon: const Icon(Icons.menu, color: Colors.white,), onPressed: () {_handleMenuOpenClose();}),
+      top: 32,
+      left: 4.0,
+    );
+  }
+
+  Widget _buildMenuContent() {
+    final List<Map> _menus = <Map>[
+      {
+        "icon": Icons.person,
+        "title": "profile",
+        "color": Colors.white,
+      },
+      {
+        "icon": Icons.view_agenda,
+        "title": "feed",
+        "color": Colors.white,
+      },
+      {
+        "icon": Icons.swap_calls,
+        "title": "activity",
+        "color": Colors.cyan,
+      },
+      {
+        "icon": Icons.settings,
+        "title": "settings",
+        "color": Colors.white,
+      }
+    ];
+    return Padding(
+      padding: const EdgeInsets.only(left: 64, top: 96),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: _menus.map((menuItem) {
+              return ListTile(
+                leading: Icon(
+                  menuItem["icon"],
+                  color: menuItem["color"],
+                ),
+                title: Text(
+                  menuItem["title"],
+                  style: TextStyle(
+                    color: menuItem["color"],
+                    fontSize: 24,
+                  ),
+                ),
+              );
+            }).toList()),
       ),
     );
   }
